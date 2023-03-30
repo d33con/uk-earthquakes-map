db6dd7dcc71076617a24276e32746dd4aaea909b
Oli Bullen <d33con@gmail.com>
2023-03-30 14:59:17 +0100

    add an explainer about popover

---
+++ src/AboutOverlay.jsx
@@ -0,0 +1,30 @@
+import Button from 'react-bootstrap/Button';
+import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
+import Popover from 'react-bootstrap/Popover';
+import Row from 'react-bootstrap/Row';
+
+const popover = (
+  <Popover id="about-popover">
+    <Popover.Header as="h5">About</Popover.Header>
+    <Popover.Body>
+      <Row>
+        <h6>Filters</h6>
+        <p>Set a date range using the date pickers</p>
+        <p>Show land or sea earthquakes, or both locations</p>
+        <p>Use the slider to filter the earthquakes by intensity</p>
+        <h6>Map</h6>
+        <p>The list of earthquakes will update as the map is moved or zoomed</p>
+        <h6>Table</h6>
+        <p>The table updates automatically and can be sorted by Date, Magnitude, Depth and Location</p>
+      </Row>
+    </Popover.Body>
+  </Popover>
+);
+
+export default function AboutOverlay() {
+  return (
+    <OverlayTrigger trigger="hover" placement="right" overlay={popover}>
+      <Button variant="primary" className="btn-about">?</Button>
+    </OverlayTrigger>
+  )
+}
--- src/Title.jsx
+++ src/Title.jsx
@@ -1,9 +1,13 @@
 import Row from 'react-bootstrap/Row';
+import AboutOverlay from './AboutOverlay';
 
 export default function Title() {
   return (
     <Row className="title-section p-4">
-      <h1 className="text-center color-main pb-4">Earthquakes in the UK 2022</h1>
+      <h1 className="text-center color-main pb-4">
+        Earthquakes in the UK 2022
+        <AboutOverlay />
+      </h1>
       <hr />
     </Row>
   )
