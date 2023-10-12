dd9a9d80c4e63db3c20b106b9bea43dbf7e84417
Oli Bullen <d33con@gmail.com>
2023-10-12 20:39:37 +0100

    setup vitest testing

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import webfontDownload from 'vite-plugin-webfont-dl';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import webfontDownload from "vite-plugin-webfont-dl";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), webfontDownload()],
-})
+  plugins: [react({ fastRefresh: false }), webfontDownload()],
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: "./setupTests.js",
+  },
+});
