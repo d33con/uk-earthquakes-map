af1bb9acb809c16440e8296dbbdbbbf1fb2c2920
Oli Bullen <d33con@gmail.com>
2023-11-02 12:17:27 +0000

    tidy up imports

--- src/components/App.test.jsx
+++ src/components/App.test.jsx
@@ -1,4 +1,4 @@
-import { render, screen, within, waitFor, act } from "@testing-library/react";
+import { render, screen, within } from "@testing-library/react";
 import { it, describe, expect } from "vitest";
 import { userEvent } from "@testing-library/user-event";
 import App from "./App";
