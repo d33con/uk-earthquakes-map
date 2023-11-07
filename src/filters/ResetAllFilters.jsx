93b2f0f2e723b412ffaa701eaefcd23cf98f1c87
Oli Bullen <d33con@gmail.com>
2023-11-07 12:22:21 +0000

    refactor

--- src/filters/ResetAllFilters.jsx
+++ src/filters/ResetAllFilters.jsx
@@ -9,11 +9,13 @@ export default function ResetAllFilters({
 }) {
   return (
     <Row className="p-4">
-      <h5 className="text-center color-main">{`Showing ${totalFiltered} of ${totalEarthquakes} total earthquakes`}</h5>
+      <h5 className="text-center color-main">
+        Showing {totalFiltered} of {totalEarthquakes} total earthquakes
+      </h5>
       <div className="text-center mb-4">
         <Button
           variant="outline-secondary"
-          disabled={totalFiltered < totalEarthquakes ? false : true}
+          disabled={totalFiltered === totalEarthquakes}
           onClick={resetFilters}
         >
           Show all
