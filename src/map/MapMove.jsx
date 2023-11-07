cf3522f8d91634a6d39649055794c4e930261249
Oli Bullen <d33con@gmail.com>
2023-11-07 12:25:47 +0000

    run getBounds function once

--- src/map/MapMove.jsx
+++ src/map/MapMove.jsx
@@ -3,10 +3,11 @@ import { useMapEvents } from "react-leaflet";
 export default function MapMove({ handleMapMove }) {
   const map = useMapEvents({
     moveend: () => {
-      const maxLatitude = parseFloat(map.getBounds().getNorth().toFixed(3));
-      const minLatitude = parseFloat(map.getBounds().getSouth().toFixed(3));
-      const maxLongitude = parseFloat(map.getBounds().getEast().toFixed(3));
-      const minLongitude = parseFloat(map.getBounds().getWest().toFixed(3));
+      const bounds = map.getBounds();
+      const maxLatitude = parseFloat(bounds.getNorth().toFixed(3));
+      const minLatitude = parseFloat(bounds.getSouth().toFixed(3));
+      const maxLongitude = parseFloat(bounds.getEast().toFixed(3));
+      const minLongitude = parseFloat(bounds.getWest().toFixed(3));
 
       handleMapMove({
         maxLatitude,
