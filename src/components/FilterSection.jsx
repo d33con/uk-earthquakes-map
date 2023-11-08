fd7d044de0a17e666e8e35fb956f99355d2e1e2c
Oli Bullen <d33con@gmail.com>
2023-11-08 12:00:58 +0000

    use Fragment shorthand

--- src/components/FilterSection.jsx
+++ src/components/FilterSection.jsx
@@ -1,6 +1,6 @@
-import React, { Fragment } from "react";
-import Row from "react-bootstrap/Row";
+import React from "react";
 import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
 import DateFilter from "../filters/DateFilter";
 import LocationFilter from "../filters/LocationFilter";
 import MagnitudeFilter from "../filters/MagnitudeFilter";
@@ -8,7 +8,7 @@ import ResetAllFilters from "../filters/ResetAllFilters";
 
 export default function FilterSection(props) {
   return (
-    <Fragment>
+    <>
       <Row className="filter-section ps-4 pe-4 pb-4" xs={1} xl={3}>
         <Col className="divider mb-4">
           <DateFilter
@@ -38,6 +38,6 @@ export default function FilterSection(props) {
         totalFiltered={props.totalFiltered}
         totalEarthquakes={props.totalEarthquakes}
       />
-    </Fragment>
+    </>
   );
 }
